f0e1b8a35bce80ad60415c4f63c795bad5d394c3
Yurii-Naumchuk <98982620+Yurii-Naumchuk@users.noreply.github.com>
2023-01-23 12:38:22 +0200

    fix token comparison

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -7,23 +7,19 @@ const { SECRET_KEY } = process.env;
 const auth = async (req, res, next) => {
   const { authorization = "" } = req.headers;
   const [bearer, token] = authorization.split(" ");
-
-  try {
-    if (bearer !== "Bearer") {
-      throw HttpError(401, "Not authorized");
+      if (bearer !== "Bearer") {
+     next(HttpError(401));
     }
+  try {
     const { id } = jwt.verify(token, SECRET_KEY);
     const user = await User.findById(id);
-    if (!user || !user.token) {
-      throw HttpError (401, "Not authorized");
+    if (!user || user.token !==token) {
+      next(HttpError(401, "Not authorized"));
     }
     req.user = user;
     next();
-  } catch (error) {
-    if (error.message === "Invalid signature") {
-      error.status = 401;
-    }
-    next(error);
+  } catch {
+    next(HttpError(401, "Not authorized"));
   }
 };
 
