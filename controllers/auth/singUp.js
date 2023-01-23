7269a372df38faef99c3b7015d3c4fba030e3b41
Yurii-Naumchuk <98982620+Yurii-Naumchuk@users.noreply.github.com>
2023-01-24 00:50:55 +0200

    done

--- controllers/auth/singUp.js
+++ controllers/auth/singUp.js
@@ -1,6 +1,7 @@
 const { User } = require("../../models/user");
 const { HttpError } = require("../../helpers");
 const gravatar = require("gravatar");
+const { v4 } = require("uuid");
 
 const singUp = async (req, res) => {
   const { email, password } = req.body;
@@ -9,9 +10,12 @@ const singUp = async (req, res) => {
     throw HttpError(409, "Email in use");
   }
   const avatarURL = gravatar.url(email);
-  const newUser = new User({ email, password, avatarURL });
+  const verificationToken = v4();
+  const newUser = new User({ email, password, avatarURL, verificationToken });
   newUser.setPassword(password);
   newUser.save();
+  const mail = createVerifyEmail(email, verificationToken);
+  await sendEmail(mail);
   res.status(201).json({
     status: "success",
     code: 201,
--- helpers/index.js
+++ helpers/index.js
@@ -1,7 +1,11 @@
 const HttpError = require("./HttpError");
 const handleErrorsSchema = require("./handleErrorsSchema");
+const createVerifyEmail = require("./createVerifyEmail");
+const sendEmail = require("./sendEmail");
 
 module.exports = {
   HttpError,
   handleErrorsSchema,
+  createVerifyEmail,
+  sendEmail,
 };
