b27ff68093befd1282686407e36b033f0ca8d5e8
Yurii-Naumchuk <98982620+Yurii-Naumchuk@users.noreply.github.com>
2023-01-23 19:58:36 +0200

    finished

--- server.js
+++ server.js
@@ -4,11 +4,12 @@ const app = require("./app");
 
 const { DB_HOST, PORT = 3000 } = process.env;
 
+mongoose.set("strictQuery", true);
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(PORT);
     console.log("Database connection successful");
+    app.listen(PORT);
   })
   .catch((error) => {
     console.log(error.message);
